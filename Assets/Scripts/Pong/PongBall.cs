4f1e6568d1b678a00334888448eb9e2a41ec2ca0
Kaleighuh <10958501@uvu.edu>
2023-10-20 15:54:44 -0600

    Physics Issues

--- Assets/Scripts/Pong/PongBall.cs
+++ Assets/Scripts/Pong/PongBall.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class PongBall : MonoBehaviour
 {
     private Rigidbody _rigidbody;
+    public float speed = 200.00f;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.bounciness = 1.0f;
     }
     private void Start()
     {
@@ -17,5 +19,8 @@ public class PongBall : MonoBehaviour
     {
         float x = Random.value < 0.5f ? -1.0f : 1.0f;
         float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1.0f);
+
+        Vector3 direction = new Vector3(x, y);
+        _rigidbody.AddForce(direction * this.speed);
     }
 }
